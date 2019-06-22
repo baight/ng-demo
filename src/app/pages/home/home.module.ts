44d4b0fba64d9ae636270904ab5a476d63b440fc
baight <303730915@qq.com>
2019-06-22 09:18:03 +0800

    update

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { MyCommonModule } from 'src/app/my-common/my-common.module';
 import { Routes, RouterModule } from '@angular/router';
+import { MyCommonModule } from 'src/app/my-common/my-common.module';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
